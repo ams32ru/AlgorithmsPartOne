21182da03d18bd52c0b6a21a61064f33032c0a58
ams32ru <ams32ru@mail.ru>
2022-11-28 21:14:12 +0300

    Выполнил дз

--- src/main/java/org/example/IntegerList/IntegerList.java
+++ src/main/java/org/example/IntegerList/IntegerList.java
@@ -7,7 +7,7 @@ import org.example.ValidateException.ValidateItemException;
 import java.util.Arrays;
 
 public class IntegerList implements ListInterface {
-    private final Integer[] storage;
+    private Integer[] storage;
     private int size;
 
 
@@ -22,7 +22,10 @@ public class IntegerList implements ListInterface {
 
     @Override
     public Integer add(Integer item) {
-        validateSize();
+//        validateSize();
+        if (size == storage.length) {
+            grow();
+        }
         validateItem(item);
         storage[size++] = item;
         return item;
@@ -30,7 +33,10 @@ public class IntegerList implements ListInterface {
 
     @Override
     public Integer add(int index, Integer item) {
-        validateSize();
+//        validateSize();
+        if (size == storage.length) {
+            grow();
+        }
         validateItem(item);
         validateIndex(index);
         if (index == size) {
@@ -147,17 +153,52 @@ public class IntegerList implements ListInterface {
         }
     }
 
+    private void grow() {
+        storage = Arrays.copyOf(storage, size + size / 2);
+    }
+
     private static void sort(Integer[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int temp = arr[i];
-            int j = i;
-            while (j > 0 && arr[j - 1] >= temp) {
-                arr[j] = arr[j - 1];
-                j--;
-            }
-            arr[j] = temp;
+        quickSort(arr, 0,arr.length -1);
+//        for (int i = 1; i < arr.length; i++) {
+//            int temp = arr[i];
+//            int j = i;
+//            while (j > 0 && arr[j - 1] >= temp) {
+//                arr[j] = arr[j - 1];
+//                j--;
+//            }
+//            arr[j] = temp;
+//        }
+    }
+
+    private static void quickSort(Integer[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
         }
     }
+
+    private static int partition(Integer[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swapElements(arr, i, j);
+            }
+        }
+        swapElements(arr, i + 1, end);
+        return i + 1;
+    }
+
+    private static void swapElements(Integer[] arr, int left, int right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+
 
     private boolean binarySearch(Integer[] arr, int item) {
         int min = 0;
@@ -181,3 +222,4 @@ public class IntegerList implements ListInterface {
 
 
 }
+
